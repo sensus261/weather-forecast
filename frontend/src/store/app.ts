9cc1dbc67cd07004625dce9221aa8dd19673def3
sensus261 <44163811+sensus261@users.noreply.github.com>
2023-02-16 23:37:39 +0200

    Update app.ts

--- frontend/src/store/app.ts
+++ frontend/src/store/app.ts
@@ -3,6 +3,6 @@ import { defineStore } from 'pinia'
 
 export const useAppStore = defineStore('app', {
   state: () => ({
-    //
+    // TODO: add global states like measure system and other vars passed to api
   }),
 })
