f9b355e5c4b13f520d3613b875c18f9058144966
Alexandru Dorin Dragan <dorin.alexandru.dragan@gmail.com>
2023-02-17 17:14:53 +0200

    issue #3 - Redis dropped

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -1,5 +1,4 @@
 export { default as logger } from './logger';
-export { default as redis } from './redis';
 export { default as validateEnv } from './validateEnv';
 
 export { isListWithoutNull } from './isListWithoutNull';
